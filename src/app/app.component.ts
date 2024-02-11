8032fb6dbbbb8541de37dff29567c2171fc4bc47
Flavio Zirkler <flavio-armando.zirkler@iu-study.org>
2024-02-11 17:03:22 +0100

    Basic structure: New components "header-nav" and "content" added. Reference to component.html and component.ts created.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+import { ContentComponent } from './content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet,
+  HeaderNavComponent,
+  ContentComponent
+  ],
+  templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
 export class AppComponent {
